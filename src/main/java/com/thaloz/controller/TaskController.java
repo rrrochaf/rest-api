240bcbfd2960c4b786a2cb6e6543709542310e2a
Ricardo Rocha <ricardorrf@gmail.com>
2024-06-28 14:31:43 -0300

    Testing

--- src/main/java/com/thaloz/controller/TaskController.java
+++ src/main/java/com/thaloz/controller/TaskController.java
@@ -1,8 +1,14 @@
 package com.thaloz.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
 
+	
+	@GetMapping("/name")
+	private String name() {
+		return "Ricardo";
+	}
 }
